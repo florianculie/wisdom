d148257c7c1ce3697b6b359e3c8b4122b3f91240
Florian Culié <florian.culie@bassetti.fr>
2024-12-27 21:33:09 +0100

    getting specific sentence is now possible
    few tweaks
    
    adding a footer, and some hovering styles
    
    Adding buttons and EE
    
    I see you APIC.
    Added refresh button
    Added Button to display the newest sentence

--- wisdom/src/app/app.component.ts
+++ wisdom/src/app/app.component.ts
@@ -1,27 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import WisdomSentencesJson from '../assets/WisdomSentences.json';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   wisdomSentences: any[] = [];
   displayedSentence: string = 'bonjour les ptits potes';
   title = 'wisdom';
 
-  constructor(){
+  constructor(private route: ActivatedRoute, private toastr: ToastrService) {
     this.wisdomSentences = WisdomSentencesJson;
-    this.displayRandomSentence();
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      // Vérifier si le paramètre 'randomIndex' existe
+      const randomIndexFromUrl = params['id'];
+      if(randomIndexFromUrl !== undefined && (randomIndexFromUrl-1 <= 0 || randomIndexFromUrl-1 >= this.wisdomSentences.length)){
+        this.toastr.warning("I See you Picaut");
+        this.displayRandomSentence();
+      } else {
+        // Si 'randomIndex' est présent, l'utiliser directement
+        const index = Number(randomIndexFromUrl) - 1;
+        if (!isNaN(index) && index >= 0 && index < this.wisdomSentences.length) {
+          this.displayedSentence = this.wisdomSentences[index].sentence;
+        } else {
+          this.displayRandomSentence(); // Si l'index est invalide, choisir un random
+        }
+      }
+    });
   }
 
   displayRandomSentence() {
+    // Générer un index aléatoire parmi les id des citations
     const randomIndex = Math.floor(Math.random() * this.wisdomSentences.length);
     this.displayedSentence = this.wisdomSentences[randomIndex].sentence;
   }
 
+  displayNewestSentence() {
+    this.displayedSentence = this.wisdomSentences[this.wisdomSentences.length-1].sentence;
+  }
 }
